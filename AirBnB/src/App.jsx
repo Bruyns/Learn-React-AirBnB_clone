8bdc9ab423f75a12c152744a143a46e17ae7695c
Bruyns <brandon.bruyns7@gmail.com>
2023-07-11 15:15:40 +0200

    reached props scrimba-6

---
+++ AirBnB/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Hero"
+import Card from "./components/Card"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+      <div>
+        <Navbar />
+        <Main />
+        <Card />
+      </div>
+    )
+}
